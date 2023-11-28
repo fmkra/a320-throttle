dc9abf8183f94aed331484608fb9d8f13b6ff9dd
kotmawszy <kotmawszy@gmail.com>
2023-11-28 22:41:58 +0100

    Add error handling to ArduinoReader

--- Software/Python/arduino.py
+++ Software/Python/arduino.py
@@ -1,6 +1,14 @@
+from enum import Enum
+
+
 class RegisterError(Exception):
     def __init__(self):
         super().__init__("Register cannot be called after reading from the device")
+
+
+class DataLengthError(Exception):
+    def __init__(self, read, expected_len):
+        super().__init__(f"Read {read} bytes, expected {expected_len} bytes")
 
 
 def register_check(func):
@@ -11,18 +19,24 @@ def register_check(func):
     return wrapper
 
 
+class DataType(Enum):
+    Uint10 = 10
+
+
 class ArduinoReader:
     def __init__(self, serial):
         self._register_finished = False
-        self.schema = []
-        self.serial = serial
+        self._schema = []
+        self._serial = serial
+        self._schema_byte_len = 0
     
     @register_check
     def registerUint10(self):
-        self.schema.append(10)
+        self._schema.append(DataType.Uint10)
+        self._schema_byte_len += 2
         
     def read(self):
-        data = self.serial.read_until(b'\0')
+        data = self._serial.read_until(b'\0')
         
         def readData(value):
             return value & 0x7f
@@ -32,8 +46,11 @@ class ArduinoReader:
 
         result = []
         i = 0
-        for size in self.schema:
-            if size == 10:
+        if self._schema_byte_len != len(data):
+            raise DataLengthError(len(data), self._schema_byte_len)
+
+        for size in self._schema:
+            if size == DataType.Uint10:
                 result.append(readUint10(data[i:i+2]))
                 i += 2
         return result
